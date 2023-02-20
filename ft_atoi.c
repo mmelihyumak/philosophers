7b768bfcd4a5cafce7aefdc2378cf0563b8e7942
mellem0110 <62307611+mellem0110@users.noreply.github.com>
2023-02-21 02:01:35 +0300

    philosophers

---
+++ ft_atoi.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muyumak <muyumak@student.42>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 00:44:55 by muyumak           #+#    #+#             */
+/*   Updated: 2023/02/21 00:44:55 by muyumak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	ft_atoi(const char *str)
+{
+	size_t	i;
+	int		sign;
+	int		number;
+
+	i = 0;
+	sign = 1;
+	number = 0;
+	while (str[i] && ((str[i] >= 9 && str[i] <= 13) || str[i] == 32))
+		i++;
+	if (str[i] == '+')
+		i++;
+	else if (str[i] == '-')
+	{
+		sign *= -1;
+		i++;
+	}
+	while (str[i] && (str[i] >= 48 && str[i] <= 57))
+	{
+		number = (number * 10) + (str[i] - 48);
+		i++;
+	}
+	number *= sign;
+	return (number);
+}
